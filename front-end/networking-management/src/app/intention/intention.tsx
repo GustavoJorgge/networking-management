9e05fc274af2bb877dc2bd5667ef04e53a13d489
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-11-10 16:32:47 -0300

    feat: setup inicial

---
+++ front-end/networking-management/src/app/intention/intention.tsx
@@ -0,0 +1,7 @@
+export function Intention() {
+    return (
+        <div>
+            <h1>Intention Page</h1>
+        </div>
+    )
+}
--- front-end/networking-management/src/app/page.tsx
+++ front-end/networking-management/src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold text-primary mb-4">
+        Bem-vindo ao Sistema de Admissão de Membros
+      </h1>
+      <p className="text-muted-foreground">
+        Faça parte de uma rede exclusiva de profissionais. Cadastre sua intenção de entrar para AG Sistemas!
+      </p>
+    </div>
   );
 }
